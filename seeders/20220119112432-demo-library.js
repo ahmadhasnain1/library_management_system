1a31724749a9820d426f017e554a877801b32a39
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-20 10:19:58 +0500

    seeders added

--- seeders/20220119112432-demo-library.js
+++ seeders/20220119112432-demo-library.js
@@ -5,11 +5,6 @@ module.exports = {
     /**
      * Add seed commands here.
      *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
     */
 
       await queryInterface.bulkInsert('Libraries', [{
@@ -21,8 +16,8 @@ module.exports = {
     /**
      * Add commands to revert seed here.
      *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
      */
+
+     await queryInterface.bulkDelete('Libraries', null, {});
   }
 };
