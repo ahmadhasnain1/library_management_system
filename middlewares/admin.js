bdbf743c5baa57dbe72a874142a20e0f13fdb343
Ahmad Hasnain <ahmadhasnain@MP15-AhmadH-SE-373.local>
2022-01-20 10:26:30 +0500

    user and admin request validations added

---
+++ middlewares/admin.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+
+const validateAdminLogin = (req, res, next) => {
+    try{
+        const schema = Joi.object().keys({
+          email: Joi.string().email().required(),
+          password: Joi.string().min(5).max(30).required()
+        });
+        const result = schema.validate(req.body); 
+        if(result.error == null)  //means valid
+          next();
+        else
+          return res.status(400).json({
+          success: false,
+          msg: result.error.details.map(i => i.message).join(',')})
+    }
+    catch(e){
+        if(!e.status) {
+          res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+        } else {
+            res.status(e.status).json( { error: { code: e.code, message: e.message } });
+        }
+    }
+  }
+
+  const validateAdminUpdate = (req, res, next) => {
+    try{
+        const schema = Joi.object().keys({
+          full_name: Joi.string().regex(/^[a-zA-Z]+$/).min(3).max(30).optional(),
+          email: Joi.string().email().optional(),
+          password: Joi.string().min(5).max(30).optional()
+        });
+        const result = schema.validate(req.body); 
+        if(result.error == null)  //means valid
+          next();
+        else
+          return res.status(400).json({
+          success: false,
+          msg: result.error.details.map(i => i.message).join(',')})
+    }
+    catch(e){
+        if(!e.status) {
+          res.status(500).json( { error: { code: 'UNKNOWN_ERROR', message: 'An unknown error occurred.' } });
+        } else {
+            res.status(e.status).json( { error: { code: e.code, message: e.message } });
+        }
+    }
+  }
+
+
+  module.exports = {
+      validateAdminLogin,
+      validateAdminUpdate
+  }
